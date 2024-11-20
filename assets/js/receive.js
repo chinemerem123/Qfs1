d3a875487ae0902b0ce45ad5480b94a629b3ea7f
Nnaji Christian <54823349+chinemerem123@users.noreply.github.com>
2024-11-20 08:58:02 +0100

    push

--- assets/js/receive.js
+++ assets/js/receive.js
@@ -1,7 +1,17 @@
-let walletAddress="0x247ABa40d166BA0C90E988d1158Bbb70B172ee6C";
-let tokenName="USDT (TRX)";
-let amount=document.getElementById("wallet-amt");
-let amt=amount.value;
-let qrcode=document.getElementById("qrImg");
 
-qrcode.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+tokenName+":"+walletAddress+"?amount="+amt;
+let amount=document.getElementById("wallet-amt");
+
+let btnTog=document.getElementById("tog-rec");
+
+
+
+
+
+btnTog.addEventListener('click', ()=>{
+
+        let amt=amount.value;
+        localStorage.setItem("payment-amt",amt);
+   
+})
+
+
